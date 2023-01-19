7349c55be14305f6510728eeefa0ababe160c6d5
Angus Wu <anguswu1022@ucla.edu>
2023-01-19 15:31:23 -0800

    Feature 14 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from receipts.views import (receipt_list, create_receipt,category_list,
-                            account_list, create_category)
+from receipts.views import (receipt_list, create_receipt, category_list,
+                            account_list, create_category, create_account)
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("categories/", category_list, name="category_list"),
     path("accounts/", account_list, name="account_list"),
     path("categories/create/", create_category, name="create_category"),
+    path("accounts/create/", create_account, name="create_account"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from receipts.models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
-from receipts.forms import ReceiptForm, ExpenseForm
+from receipts.forms import ReceiptForm, ExpenseForm, AccountForm
 
 
 # Create your views here.
@@ -69,3 +69,22 @@ def create_category(request):
         "form": form,
     }
     return render(request, "categories/create.html", context)
+
+
+@login_required
+def create_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(False)
+            account.owner = request.user
+            account.save()
+
+            return redirect("account_list")
+
+    else:
+        form = AccountForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/create.html", context)
